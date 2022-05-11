1caadd19d2af6e62211cf4945960485c2149bb34
Diego Cavalcanti <diegotimao1104@gmail.com>
2022-05-10 21:37:46 -0300

    Envio de requisito 7 Concluído

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -42,12 +42,35 @@ describe('Teste do componente PokemonDetails', () => {
       name: /location/i,
     });
 
+    const url1 = 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png';
+    const url2 = 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png';
+
     const descriptionLocation = screen.getAllByText(/Kanto/i);
+
     expect(descriptionLocation[0]).toBeInTheDocument();
     expect(descriptionLocation[1]).toBeInTheDocument();
     expect(location[0]).toBeInTheDocument();
     expect(location[1]).toBeInTheDocument();
     expect(location[0].alt).toBe('Pikachu location');
     expect(location[1].alt).toBe('Pikachu location');
+    expect(location[0].src).toBe(url1);
+    expect(location[1].src).toBe(url2);
+  });
+
+  test('3 - Teste se o usuário pode favoritar um pokémon na página de detalhes', () => {
+    renderWithRouter(<App />);
+
+    const link = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(link);
+
+    const linkFavorites = screen.getByText(/pokémon favoritado\?/i);
+    expect(linkFavorites).toBeInTheDocument();
+
+    userEvent.click(linkFavorites);
+    const favorites = screen.getByRole('img', { name: /pikachu is marked as favorite/i });
+    expect(favorites).toBeInTheDocument();
+
+    userEvent.click(linkFavorites);
+    expect(favorites).not.toBeInTheDocument();
   });
 });
